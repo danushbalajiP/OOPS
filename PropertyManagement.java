030c1dadf7ce4d9994f26b1c577833db9ce06d75
danushbalajiP <danushprasath30@gmail.com>
2026-02-12 15:09:07 +0530

    Add files via upload

---
+++ PropertyManagement.java
@@ -0,0 +1,39 @@
+public class Father{
+private int landarea;
+private String propertyType;
+public void showproperty(string sp){
+showproperty=sp;
+}
+
+public class ChildOne extends Father{
+private String usage;
+public void showUsage(string sw){
+showUsage=sw;
+}
+
+public class ChildTwo extends Father{
+private String usage;
+public void showUsage(string us){
+showUsage=us;
+{
+
+
+
+
+public class PropertyManagement{
+public class void main(String[] arg){
+ChildOne c1 = new ChildOne();
+c1.landarea = 2000 ;
+c1.property = "Ancestral House";
+c1.usage = "Residential Purpose";
+System.out.println("childone details");
+c1.showusage();
+
+system.out.println();
+c2.landarea = 2000 ;
+c2.property = "Ancestral House";
+c2.usage = "Residential Purpose";
+System.out.println("childtwo details");
+c2.showusage();
+}
+}
